b6ffccf11d3266654b146880fb5568c16ef8980b
Jeniffer Da Silva Venâcio Faria <83260657+venanciofaria@users.noreply.github.com>
2022-08-07 08:33:51 -0300

    Add files via upload

---
+++ function-return/script.js
@@ -0,0 +1,26 @@
+
+
+function soma (num1, num2) {
+    return num1 + num2
+}
+
+let resultado = soma(10, 5)
+
+console.log(resultado)
+document.write(resultado)
+
+
+
+
+/************************************************ */
+
+
+function milhasEmKm(milhas) {
+    let km = milhas * 1.6
+
+    return km
+}
+
+let result = milhasEmKm(150)
+
+console.log(result)
